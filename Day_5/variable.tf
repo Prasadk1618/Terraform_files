3cb313d25bda3d01e57d19eb22b5d79a911439c6
PRASAD KALE <prasadkale1618@gmail.com>
2025-02-05 18:32:43 +0530

    Create variable.tf

---
+++ Day_5/variable.tf
@@ -0,0 +1,28 @@
+variable "my_region" {
+   type = string
+}
+
+
+##variable "access_key" {
+##   type = string
+##}
+
+
+##variable "secret_key" {
+##   type = string
+##}
+
+
+variable "my_ami" {
+   type = string
+}
+
+
+variable "vpc_id" {
+  type = string
+}
+
+
+variable "inst_type" {
+   type = string
+}
